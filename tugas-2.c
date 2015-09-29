beff37e59722c2940974665e0d75099723e31f9f
d400150126 <rizar77@gmail.com>
2015-09-29 09:43:40 +0700

    Update tugas-2.c

--- tugas-2.c
+++ tugas-2.c
@@ -4,11 +4,11 @@ int main ()
 {
     int a = 1,b = 5,c = 4;
     float d, X1, X2, X;
-    printf("akar-akar persamaan kuadrat ax*x+bx+c=0\n");
+    printf("akar-akar persamaan kuadrat aX*X+bX+c=0\n");
     printf("Masukkan nilai koefisien a, b, dan c:\n");
 d = b*b-4*a*c;
 printf("Diskriminan = %g\n", d);
-printf("Akar persamaan %iX*x + %iX + %i = 0 adalah:\n", a,b,c);
+printf("Akar persamaan %iX*X + %iX + %i = 0 adalah:\n", a,b,c);
 if (d > 0) {
 X1 = (-b+sqrt(d))/(2*a);
 X2 = (-b-sqrt(d))/(2*a);
